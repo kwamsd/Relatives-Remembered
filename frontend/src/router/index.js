5109d502b896c12dfa4e48a1ebd1bb41b344c76a
Maxime <maxime.bernheim@efrei.net>
2025-05-27 13:20:49 +0100

    Mes modifications sur le router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,12 +3,14 @@ import Home from '../views/Home.vue'
 import AboutUs from '../views/AboutUs.vue'
 import List from '../views/List.vue'
 import Login from '../views/Login.vue'
+import Signup from '../views/Signup.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/about', component: AboutUs },
   { path: '/list', component: List },
-  { path: '/login', component: Login}
+  { path: '/login', component: Login},
+  { path: '/signup', component: Signup}
 ]
 
 const router = createRouter({
