a9c71b8f7cc65635918f058d4ca2cbc0c589727c
Maxime <maxime.bernheim@efrei.net>
2025-06-02 10:52:32 +0100

    quelques routes en plus

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,19 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
+const templateRoutes = require('./routes/template');
+const deceasedRoutes = require('./routes/deceased');
+const anecdoteRoutes = require('./routes/anecdotes');
+const backgroundRoutes = require('./routes/backgrounds');
+
+app.use('/api/deceased', deceasedRoutes);
+app.use('/api', anecdoteRoutes);
+app.use('/api', backgroundRoutes);
+
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use('/api/templates', templateRoutes);
 
 // // Routes
 // const deceasedRoutes = require('./routes/deceased');
